7c2ef6b845dc2a9dbb5838f7e081162d3757c794
M. Fatih B <m.fatih.bostanci@gmail.com>
2021-12-23 15:32:59 +0100

    began module for gameboard

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // Module for Gameboard
+var gameBoard = (function(){
+    var state = [["","",""],["","",""],["","",""]];
+
+    function visualize(){}
+
+    function setState(){}
+
+    function getState(){
+        return state;
+    }
+    return {getState};
+})();
 
 // Factory for players
