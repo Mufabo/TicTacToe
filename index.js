f227ead1a15a5e2d9a8b3f2ed488fbc239cf0c5f
FatihB <fatih.bostanci@accenture.com>
2021-12-24 09:50:28 +0100

    set and get

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 // Module for Gameboard
-var gameBoard = (function(){
-    var state = [["","",""],["","",""],["","",""]];
+var gameBoard = (function() {
+    var state = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
 
-    function visualize(){}
+    function visualize() {}
 
-    function setState(){}
+    function setState(row, col, val) {
+        if (state[row][col] === "") state[row][col] = val;
+    }
 
-    function getState(){
+    function getState() {
         return state;
     }
-    return {getState};
+    return { getState, setState, visualize };
 })();
 
 // Factory for players
+
+function switchScreen(screen) {
+    if (screen.id === "startscreen") {
+        document.getElementById("startscreen").style.display = "none";
+        document.getElementById("gamescreen").style.display = "inline";
+    } else {
+        document.getElementById("startscreen").style.display = "inline";
+        document.getElementById("gamescreen").style.display = "none";
+    }
+}
